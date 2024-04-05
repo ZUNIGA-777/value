60458393157579f381947e089c49e1e604840665
ZUNIGA777 <142277232+ZUNIGA-777@users.noreply.github.com>
2024-04-05 15:16:42 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("#entrada")
+const boton = document.querySelector("#btn")
+
+boton.addEventListener("click", ()=>{
+    const valorEntrada = input.value
+    alert(`Hola, ${valorEntrada}!!!`)
+})
